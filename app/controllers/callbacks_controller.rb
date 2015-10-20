90ec5c01928940f8c998add211189f2b135952bb
adevas <adevanir@gmail.com>
2015-10-20 18:57:05 +0000

    Authentication with facebook and google

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -10,9 +10,9 @@ class CallbacksController < ApplicationController
     sign_in_and_redirect @user
   end
 
-  def google
+  def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
-  
+
 end
